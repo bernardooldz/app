2dd0f4a79713b423361f3359dab45bd61f83eb60
Bernardo Diniz <bernardoldiniz04@gmail.com>
2024-09-09 22:24:32 -0300

    fundamentos 003

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-// arrays, objetos
 let meta = {
     value: 'ler um livro por mês',
-    checked: false,
-    log: (info) => {
-        console.log(info)
-    }
+    checked: true,
 }
 
-meta.value = "não é mais ler um livro"
-meta.log(meta.value)
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos todods os dias",
+        checked: false
+    }
+]
 
-// function // arrow function
-// const criarMeta = () => {}
-
-// function criarMeta() {}
+console.log(metas[1].value)
